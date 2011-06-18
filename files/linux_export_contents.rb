64a1bb0ed084ab666df29a73b9db8f40009dada1
Carl Caum <ccaum@Carl-Caums-MacBook-Pro.local>
2011-06-17 19:22:41 -0700

    About to be merged over

--- files/linux_export_contents.rb
+++ files/linux_export_contents.rb
@@ -1,71 +1,72 @@
-#/usr/bin/env ruby
+#!/usr/bin/env ruby
 
 require 'yaml'
 
 exports = Hash.new
 
 class Export
-	attr_accessor :name, :hosts
+  attr_accessor :name, :hosts
 
-	def initialize(params)
-		@name  = params[:name]
-		@hosts = Array.new
+  def initialize(params)
+    @name  = params[:name]
+    @hosts = Array.new
 
-		params[:host].to_a.each do |host|
-			@hosts << Host.new :name => host, 
-				:host_parameters => params[:parameters], 
-				:subnet          => params[:subnet]
-		end
-	end
+    params[:host].to_a.each do |host|
+      @hosts << Host.new( :name => host, 
+        :host_parameters => params[:parameters], 
+        :subnet          => params[:subnet]
+      )   
+    end 
+  end 
 
-	def to_s
-		"#{@name}\t" + hosts.map {|host| host.to_s }.join(' ')
-	end
+  def to_s
+    "#{@name}\t" + hosts.map {|host| host.to_s }.join(' ')
+  end 
 end
 
 class Host
-	attr_accessor :host_parameters, :name, :subnet
+  attr_accessor :host_parameters, :name, :subnet
 
-	def initialize(params)
-		@name       = params[:name]
-		@parameters = params[:host_parameters]
-		@subnet     = params[:subnet]
-	end
+  def initialize(params)
+    @name       = params[:name]
+    @parameters = params[:host_parameters]
+    @subnet     = params[:subnet]
+  end 
 
-	def to_s
-		@subnet = "/#{@subnet}" unless @subnet == :undef
+  def to_s
+    @subnet = "/#{@subnet}" unless @subnet == :undef
 
-		"#{@name}#{@subnet}(#{@parameters.join(',')})"
+    "#{@name}#{@subnet}(#{@parameters.join(',')})"
 end
 
 #Process the yaml files and
 def init
-	Dir["#{ARGV[1]}/**/*.yaml"].each do |yaml|
-		resource = YAML.load( IO.read( yaml ) )
+  Dir["#{ARGV[1]}/**/*.yaml"].each do |yaml|
+    resource = YAML.load( IO.read( yaml ) ) 
 
-		# The export might be a string, so ensure array
-		resource['export'].to_a.each do |exp|
-			exports[exp] = Export :name => exp,
-				:parameters => resource['parameters'],
-				:host       => resource['host']
-		end
-	end
+    # The export might be a string, so ensure array
+    resource['export'].to_a.each do |exp|
+      exports[exp] = Export :name => exp,
+        :parameters => resource['parameters'],
+        :host       => resource['host']
+    end 
+  end 
 end
 
 def contents
-	exports.values.map { |export|
-		export.to_s
-	}.join("\n")
+  exports.values.map { |export|
+    export.to_s
+  }.join("\n")
 end
 
 #Call init
 init
 
 case ARGV[0]
-when "apply"
-	file.open('/etc/exports', 'w') { |f|
-		f.write contents
-	}
-when "check"
-	IO.read( '/etc/exports' ) == contents
+when 'apply'
+  file.open('/etc/exports', 'w') { |f| 
+    f.write contents
+  }
+when 'check'
+  IO.read( '/etc/exports' ) == contents
 end
